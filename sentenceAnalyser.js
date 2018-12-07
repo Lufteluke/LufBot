e21eb7024b660f0c1fbf8536320eb88fc21be403
AM\Lufteluke <lufteluke@corrupted.systems>
2018-12-08 00:16:53 +0100

    dd

--- sentenceAnalyser.js
+++ sentenceAnalyser.js
@@ -12,7 +12,7 @@ module.exports.parse = function (message) {
     returnVar = r.default(message.from.first_name)
   }
   if (h.matchWordFromList(message.text.toLowerCase(), c.owo)){
-    returnVar = r.owo(exports.parser(message))
+    returnVar = r.owo(returnVar)
   }
   
   return h.capitaliseFirst(returnVar)
