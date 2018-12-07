258c4eb2d5ba20ac2ab1f9e636acae3adeeef504
AM\Lufteluke <lufteluke@corrupted.systems>
2018-12-07 22:57:33 +0100

    stpd

--- sentenceAnalyser.js
+++ sentenceAnalyser.js
@@ -7,8 +7,8 @@ module.exports.parse = function (message) {
   
   var returnVar = exports.parser(message)
   
-  if (returnVar.length == 0) {
-    r.default(message.from.first_name)
+  if (returnVar.length === 0) {
+    returnVar = r.default(message.from.first_name)
   }
   if (h.matchWordFromList(message.text.toLowerCase(), c.owo)){
     returnVar = r.owo(exports.parser(message))
