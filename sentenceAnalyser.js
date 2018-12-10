077eb9e10ed2bc0e021c689fad473ce418ea3393
AM\Lufteluke <lufteluke@corrupted.systems>
2018-12-10 22:21:27 +0100

    forgot to add to command

--- sentenceAnalyser.js
+++ sentenceAnalyser.js
@@ -27,7 +27,7 @@ function parser (message) {
   const commandWithName = split[0].toLowerCase()
   const command = commandWithName.split('@')[0] //because it might be /talk@lufbot
   
-  if ((command !== null) && h.matchWordFromList(command, c.commands)) {
+  if ((command !== null) && h.matchWordFromList(command, c.commands.concat('/yiff'))) {
     clean = clean.replace(commandWithName, '') //we don't want the name
     
     console.log('Command: ' + command)
